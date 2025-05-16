cdf13ecc39cf3bd74ab3bf1f38927cecb044105e
pannati <pavanpannati8@gmail.com>
2025-05-16 13:05:20 +0530

    image scraping

--- image_scrape.py
+++ image_scrape.py
@@ -1,15 +1,21 @@
+# image scraping from PINTREST
+# requirements install
+#!pip install requests selenium
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as chromeservice
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 import requests
+
 search = input('Search Images : ')     #search image names what you need
 images_count = int(input('Number of Images Downloaded : '))   #search images how many you need
 driver = webdriver.Chrome(service=chromeservice("E:\\chromedriver-win64\\chromedriver.exe"))
 #if you doesnot have chrome driver installed you can use below commented code , it might need high speed internet
 #driver = webdriver.Chrome(service=chromeservice(ChromeDriverManager().install()))
 URL = f'https://in.pinterest.com/search/pins/?q={search}&rs=autocomplete_bubble&b_id=BF8EaZQQ6XHOAAAAAAAAAAALis344AohI_ad3R-r068A-Gh6ML70ygUq9g_rAAO0lNVTL8nwo6lPncrZ4UTdeIznbfO4Nw7g_nfiLrSKq0myEv3MCJr6TV8&source_id=Q7iDh4c0&top_pin_id=2533343532482543'
+
 
 driver.get(URL)
 image_urls = []
